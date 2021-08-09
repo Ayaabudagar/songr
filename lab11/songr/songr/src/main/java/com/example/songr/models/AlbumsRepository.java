2ba5ec7fd009fc8f712d364623019e1a8fdbbfd8
Ayaabudagar <ayakhlil23@yahoo.com>
2021-08-10 02:32:29 +0300

    done

---
+++ lab11/songr/songr/src/main/java/com/example/songr/models/AlbumsRepository.java
@@ -0,0 +1,9 @@
+package com.example.songr.models;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlbumsRepository extends CrudRepository<Albums,Integer> {
+
+
+}
